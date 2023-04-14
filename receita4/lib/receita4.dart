b0cf2710de1befb724999dc2b3666744037843ce
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2023-04-14 08:13:26 -0300

    espalhados

--- receita4/lib/receita4.dart
+++ receita4/lib/receita4.dart
@@ -25,7 +25,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final dataObjects = [    {"name": "Cerveja 1", "style": "Estilo 1", "ibu": "10"},    {"name": "Cerveja 2", "style": "Estilo 2", "ibu": "20"},    {"name": "Cerveja 3", "style": "Estilo 3", "ibu": "30"},    {"name": "Cerveja 4", "style": "Estilo 4", "ibu": "40"},    {"name": "Cerveja 5", "style": "Estilo 5", "ibu": "50"},    {"name": "Cerveja 6", "style": "Estilo 6", "ibu": "60"},    {"name": "Cerveja 7", "style": "Estilo 7", "ibu": "70"},    {"name": "Cerveja 8", "style": "Estilo 8", "ibu": "80"},    {"name": "Cerveja 9", "style": "Estilo 9", "ibu": "90"},    {"name": "Cerveja 10", "style": "Estilo 10", "ibu": "100"},  ];
+  final dataObjects = [
+    {"name": "Cerveja 1", "style": "Estilo 1", "ibu": "10"},
+    {"name": "Cerveja 2", "style": "Estilo 2", "ibu": "20"},
+    {"name": "Cerveja 3", "style": "Estilo 3", "ibu": "30"},
+    {"name": "Cerveja 4", "style": "Estilo 4", "ibu": "40"},
+    {"name": "Cerveja 5", "style": "Estilo 5", "ibu": "50"},
+    {"name": "Cerveja 6", "style": "Estilo 6", "ibu": "60"},
+    {"name": "Cerveja 7", "style": "Estilo 7", "ibu": "70"},
+    {"name": "Cerveja 8", "style": "Estilo 8", "ibu": "80"},
+    {"name": "Cerveja 9", "style": "Estilo 9", "ibu": "90"},
+    {"name": "Cerveja 10", "style": "Estilo 10", "ibu": "100"},
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -34,43 +45,28 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: DataBodyWidget(dataObjects: dataObjects),
+        child: MyTileWidget(dataObjects: dataObjects),
       ),
     );
   }
 }
 
-class DataBodyWidget extends StatelessWidget {
+class MyTileWidget extends StatelessWidget {
   final List<Map<String, String>> dataObjects;
 
-  DataBodyWidget({required this.dataObjects});
+  MyTileWidget({required this.dataObjects});
 
   @override
   Widget build(BuildContext context) {
-    final columnNames = ["Nome", "Estilo", "IBU"];
-    final propertyNames = ["name", "style", "ibu"];
-
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: DataTable(
-        columns: columnNames
-            .map((name) => DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      name,
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ),
-                ))
-            .toList(),
-        rows: dataObjects
-            .map((obj) => DataRow(
-                  cells: propertyNames
-                      .map((propName) => DataCell(Text(obj[propName] ?? '')))
-                      .toList(),
-                ))
-            .toList(),
-      ),
+    return ListView.builder(
+      itemCount: dataObjects.length,
+      itemBuilder: (BuildContext context, int index) {
+        final obj = dataObjects[index];
+        return ListTile(
+          title: Text(obj['name'] ?? ''),
+          subtitle: Text('Estilo: ${obj['style'] ?? ''} - IBU: ${obj['ibu'] ?? ''}'),
+        );
+      },
     );
   }
 }
