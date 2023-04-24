e516e51af7b9027567b9ab3030f40003d595b9a2
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2023-04-24 12:19:32 -0300

    flutter project tarefa6 -> adicionando snack bar depois da do formulário pronto

--- tarefa6/lib/main.dart
+++ tarefa6/lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    Builder(
+      builder: (BuildContext context) => MyApp(context),
+    ),
+  );
 }
+
 
 class MyApp extends StatelessWidget {
   final TextEditingController _controladorNome = TextEditingController();
   final TextEditingController _controladorQuantidade = TextEditingController();
   final TextEditingController _controladorValor = TextEditingController();
+  final BuildContext context;
+
+  MyApp(this.context);
+
 
   @override
   Widget build(BuildContext context) {
@@ -16,92 +25,101 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('CADASTRO DE PRODUTO'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Nome'),
-                  TextField(
-                    controller: _controladorNome,
-                  ),
-                ],
-              ),
-              SizedBox(height: 16.0),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Quantidade(INT)'),
-                        TextField(
-                          controller: _controladorQuantidade,
-                        ),
-                      ],
+        body: Builder(
+          builder: (BuildContext scaffoldContext) => Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Nome'),
+                    TextField(
+                      controller: _controladorNome,
                     ),
-                  ),
-                  SizedBox(width: 16.0),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Valor(FLOAT)'),
-                        TextField(
-                          controller: _controladorValor,
-                        ),
-                      ],
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Quantidade(INT)'),
+                          TextField(
+                            controller: _controladorQuantidade,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16.0),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: ElevatedButton(
-                      child: Text('Limpar'),
-                      onPressed: () {
-                        _controladorNome.clear();
-                        _controladorQuantidade.clear();
-                        _controladorValor.clear();
-                      },
+                    SizedBox(width: 16.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Valor(FLOAT)'),
+                          TextField(
+                            controller: _controladorValor,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 16.0),
-                  Expanded(
-                    child: ElevatedButton(
-                      child: Text('Dados de exemplo'),
-                      onPressed: () {
-                        _controladorNome.text = 'Produto exemplo';
-                        _controladorQuantidade.text = '10';
-                        _controladorValor.text = '9.99';
-                      },
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: ElevatedButton(
+                        child: Text('Limpar'),
+                        onPressed: () {
+                          _controladorNome.clear();
+                          _controladorQuantidade.clear();
+                          _controladorValor.clear();
+                        },
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                child: Text('Cadastrar'),
-                onPressed: () {
-                  final String nome = _controladorNome.text;
-                  final int quantidade =
-                      int.tryParse(_controladorQuantidade.text) ?? 0;
-                  final double valor =
-                      double.tryParse(_controladorValor.text) ?? 0.0;
-                  final Produto produtoNovo =
-                      Produto(nome, quantidade, valor);
-                  print(produtoNovo);
-                },
-              ),
-            ],
+                    SizedBox(width: 16.0),
+                    Expanded(
+                      child: ElevatedButton(
+                        child: Text('Dados de exemplo'),
+                        onPressed: () {
+                          _controladorNome.text = 'Produto exemplo';
+                          _controladorQuantidade.text = '10';
+                          _controladorValor.text = '9.99';
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  child: Text('Cadastrar'),
+                  onPressed: () {
+                    _processarFormulario(scaffoldContext);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _processarFormulario(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Formulário sendo processado...'),
+      ),
+    );
+
+    final String nome = _controladorNome.text;
+    final int quantidade = int.tryParse(_controladorQuantidade.text) ?? 0;
+    final double valor = double.tryParse(_controladorValor.text) ?? 0.0;
+    final Produto produtoNovo = Produto(nome, quantidade, valor);
+    print(produtoNovo);
   }
 }
 
