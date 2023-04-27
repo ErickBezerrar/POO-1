0e50eae21043ae0cb01db3be612d24bcf52493ec
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2023-04-27 09:08:48 -0300

    flutter receita5 -> adicionando estados da tela principal

--- receita5/lib/main.dart
+++ receita5/lib/main.dart
@@ -1,9 +1,129 @@
-import 'package:myapp/receita5.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+var dataObjects = [];
 
 void main() {
-  
-  var minhaReceita = Receita();
+  MyApp app = MyApp();
+  runApp(app);
+}
 
-  minhaReceita.exibirReceita();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // print("no build da classe MyApp");
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Dicas"),
+        ),
+        body: DataTableWidget(jsonObjects: dataObjects),
+        bottomNavigationBar: MyStatefulWidget(),
+      )
+    );
+  }
+}
+
+class NewNavBar extends HookWidget {
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    var selectedIndex = useState(0);
+
+    return BottomNavigationBar(
+
+      currentIndex: selectedIndex.value,
+      
+      onTap: (index) => selectedIndex.value = index,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas",
+          icon: Icon(Icons.local_drink_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Nações",
+          icon: Icon(Icons.flag_outlined),
+        ),
+      ],
+    );
+  }
+}
+
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => NewNavBar2();
+}
+
+class NewNavBar2 extends State<MyStatefulWidget> {
+  NewNavBar2();
+
+  int _selectIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _selectIndex,
+      onTap: onItemTapped,
+      selectedItemColor: Colors.teal,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas", 
+          icon: Icon(Icons.local_drink_outlined)
+        ),
+        BottomNavigationBarItem(
+          label: "Nações", 
+          icon: Icon(Icons.flag_outlined)
+        )
+      ]
+    );
+  }
+}
+
+class DataTableWidget extends StatelessWidget {
+  final List jsonObjects;
+  DataTableWidget({this.jsonObjects = const []});
+
+  @override
+  Widget build(BuildContext context) {
+    // print("no build da classe DataTableWidget");
+    var columnNames = ["Nome", "Estilo", "IBU"],
+        propertyNames = ["name", "style", "ibu"];
+
+    return DataTable(
+      columns: columnNames.map(
+        (name) => DataColumn(
+          label: Expanded(
+            child: Text(name,style: TextStyle(fontStyle: FontStyle.italic))
+          )
+        )
+      ).toList(),
+      rows: jsonObjects.map(
+        (obj) => DataRow(
+          cells: propertyNames.map(
+            (propName) => DataCell(Text(obj[propName]))
+          ).toList()
+        )
+      ).toList());
+  }
 }
