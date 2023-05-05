ae74d9cac29cdf3c5ebe1fbb11700dea290eabb6
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2023-05-04 22:16:15 -0300

    dakdnkand

--- receita6/lib/main.dart
+++ receita6/lib/main.dart
@@ -2,61 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class DataService {
-  final ValueNotifier<List> tableStateNotifier = new ValueNotifier([]);
+  final ValueNotifier<List> tableStateNotifier = ValueNotifier([]);
 
   final Map<int, List<Map<String, String>>> _dataMap = {
     0: [
-      {
-        "name": "Café brasileiro",
-        "type": "Arábica",
-        "strength": "Médio"
-      },
-      {
-        "name": "Café colombiano",
-        "type": "Arábica",
-        "strength": "Forte"
-      },
-      {
-        "name": "Café turco",
-        "type": "Robusta",
-        "strength": "Muito forte"
-      }
-    ],
-    
+      {"name": "Café brasileiro", "type": "Arábica", "strength": "Médio"},
+      {"name": "Café colombiano", "type": "Arábica", "strength": "Forte"},
+      {"name": "Café turco", "type": "Robusta", "strength": "Muito forte"},
+      {"name": "Café peruano", "type": "Arábica", "strength": "Médio"},
+      {"name": "Café etíope", "type": "Arábica", "strength": "Forte"},
+      {"name": "Café jamaicano", "type": "Arábica", "strength": "Médio"},
+      {"name": "Café indonésio", "type": "Robusta", "strength": "Forte"},
+      {"name": "Café guatemalteco", "type": "Arábica", "strength": "Forte"},
+      {"name": "Café mexicano", "type": "Arábica", "strength": "Suave"},
+      {"name": "Café hondurenho", "type": "Robusta", "strength": "Médio"}
+      ],
     1: [
-      {
-        "name": "La Fin Du Monde",
-        "style": "Bock",
-        "ibu": "65"
-      },
-      {
-        "name": "Sapporo Premiume",
-        "style": "Sour Ale",
-        "ibu": "54"
-      },
-      {
-        "name": "Duvel",
-        "style": "Pilsner",
-        "ibu": "82"
-      }
-    ],
-
+      {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
+      {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
+      {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
+      {"name": "Guinness Draught", "style": "Irish Dry Stout", "ibu": "45"},
+      {"name": "Tripel Karmeliet", "style": "Tripel", "ibu": "20"},
+      {"name": "Rochefort 10", "style": "Quadrupel", "ibu": "27"},
+      {"name": "Weihenstephaner Hefeweissbier", "style": "Hefeweizen", "ibu": "14"},
+      {"name": "Founders Kentucky Breakfast Stout", "style": "Imperial Stout", "ibu": "70"},
+      {"name": "Stone IPA", "style": "American IPA", "ibu": "77"},
+      {"name": "Belhaven Scottish Ale", "style": "Scottish Ale", "ibu": "20"}
+      ],
     2: [
-      {
-        "name": "Brasil",
-        "continent": "América do Sul",
-        "population": "213,3 milhões"
-      },
-      {
-        "name": "Japão",
-        "continent": "Ásia",
-        "population": "126,5 milhões"
-      },
-      {
-        "name": "Itália",
-        "continent": "Europa",
-        "population": "60,4 milhões"
-      }
+      {"name": "Brasil", "continent": "América do Sul", "population": "213.8 milhões"},
+      {"name": "China", "continent": "Ásia", "population": "1.4 bilhões"},
+      {"name": "Índia", "continent": "Ásia", "population": "1.3 bilhões"},
+      {"name": "Estados Unidos", "continent": "América do Norte", "population": "328.2 milhões"},
+      {"name": "Indonésia", "continent": "Ásia", "population": "267.7 milhões"},
+      {"name": "Paquistão", "continent": "Ásia", "population": "220.9 milhões"},
+      {"name": "Bangladesh", "continent": "Ásia", "population": "164.7 milhões"},
+      {"name": "Nigéria", "continent": "África", "population": "206.1 milhões"},
+      {"name": "Rússia", "continent": "Europa/Ásia", "population": "144.5 milhões"},
+      {"name": "Japão", "continent": "Ásia", "population": "126.5 milhões"},
+      {"name": "México", "continent": "América do Norte", "population": "128.9 milhões"},
+      {"name": "Filipinas", "continent": "Ásia", "population": "108.1 milhões"},
+      {"name": "Etiópia", "continent": "África", "population": "114.9 milhões"},
+      {"name": "Egito", "continent": "África", "population": "101.7 milhões"}
+
     ],
   };
 
@@ -65,21 +53,17 @@ class DataService {
   }
 }
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  
   @override
-  
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   final DataService dataService = DataService();
-
   int _selectedIndex = 0;
 
   static const List<Map<String, dynamic>> _navBarItems = [
@@ -89,14 +73,12 @@ class _MyAppState extends State<MyApp> {
       'columnNames': ['Nome', 'Tipo', 'Intensidade'],
       'propertyNames': ['name', 'type', 'strength'],
     },
-    
     {
       'label': 'Cervejas',
       'icon': Icon(Icons.local_drink_outlined),
       'columnNames': ['Nome', 'Estilo', 'IBU'],
       'propertyNames': ['name', 'style', 'ibu'],
     },
-    
     {
       'label': 'Nações',
       'icon': Icon(Icons.flag_outlined),
@@ -106,7 +88,6 @@ class _MyAppState extends State<MyApp> {
   ];
 
   @override
-  
   void initState() {
     super.initState();
     dataService.carregar(_selectedIndex);
@@ -118,6 +99,7 @@ class _MyAppState extends State<MyApp> {
       dataService.carregar(_selectedIndex);
     });
   }
+
 
   @override
   
