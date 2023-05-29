f816f647ef9dbfc38265aeda733ab124ec2d0a6b
ErickBezerrar <erickbrtrindade@gmail.com>
2023-05-29 10:38:41 -0300

    receita8

--- receita8/lib/main.dart
+++ receita8/lib/main.dart
@@ -10,6 +10,7 @@ class DataService {
       ValueNotifier({
     'status': TableStatus.idle,
     'dataObjects': [],
+    'columnNames': ["Nome", "Estilo", "IBU"],
   });
 
   void carregar(index) {
@@ -18,50 +19,109 @@ class DataService {
     tableStateNotifier.value = {
       'status': TableStatus.loading,
       'dataObjects': [],
+      'columnNames': [],
     };
 
     funcoes[index]();
   }
 
   void carregarCafes() {
-    return;
-  }
-
-  void carregarNacoes() {
-    return;
-  }
-
- void carregarCervejas(){
-
-    var beersUri = Uri(
-
+    var cafesUri = Uri(
       scheme: 'https',
-
       host: 'random-data-api.com',
+      path: 'api/coffee/random_coffee',
+      queryParameters: {'size': '5'},
+    );
 
-      path: 'api/beer/random_beer',
-
-      queryParameters: {'size': '5'});
-
-
-
-    http.read(beersUri).then( (jsonString){
-
-      var beersJson = jsonDecode(jsonString);
-
+    http.get(cafesUri).then((response) {
+      if (response.statusCode == 200) {
+        var cafesJson = jsonDecode(response.body);
+        tableStateNotifier.value = {
+          'status': TableStatus.ready,
+          'dataObjects': cafesJson,
+          'columnNames': ["Nome", "Origem", "Intensidade"],
+        };
+      } else {
+        tableStateNotifier.value = {
+          'status': TableStatus.error,
+          'dataObjects': [],
+          'columnNames': [],
+        };
+      }
+    }).catchError((error) {
       tableStateNotifier.value = {
-
-        'status': TableStatus.ready,
-
-        'dataObjects': beersJson,
-
-        'propertyNames': ["name","style","ibu"]
-
+        'status': TableStatus.error,
+        'dataObjects': [],
+        'columnNames': [],
       };
-
-    }); 
-
+    });
   }
+
+  Future<void> carregarNacoes() async {
+    var nationsUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/nation/random_nation',
+      queryParameters: {'size': '5'},
+    );
+
+    try {
+      var response = await http.get(nationsUri);
+      if (response.statusCode == 200) {
+        var nationsJson = jsonDecode(response.body);
+        tableStateNotifier.value = {
+          'status': TableStatus.ready,
+          'dataObjects': nationsJson,
+          'columnNames': ["Nome", "Capital", "População"],
+        };
+      } else {
+        tableStateNotifier.value = {
+          'status': TableStatus.error,
+          'dataObjects': [],
+          'columnNames': [],
+        };
+      }
+    } catch (error) {
+      tableStateNotifier.value = {
+        'status': TableStatus.error,
+        'dataObjects': [],
+        'columnNames': [],
+      };
+    }
+  }
+
+  void carregarCervejas() {
+    var beersUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/beer/random_beer',
+      queryParameters: {'size': '5'},
+    );
+
+    http.get(beersUri).then((response) {
+      if (response.statusCode == 200) {
+        var beersJson = jsonDecode(response.body);
+        tableStateNotifier.value = {
+          'status': TableStatus.ready,
+          'dataObjects': beersJson,
+          'columnNames': ["Nome", "Estilo", "IBU"],
+        };
+      } else {
+        tableStateNotifier.value = {
+          'status': TableStatus.error,
+          'dataObjects': [],
+          'columnNames': [],
+        };
+      }
+    }).catchError((error) {
+      tableStateNotifier.value = {
+        'status': TableStatus.error,
+        'dataObjects': [],
+        'columnNames': [],
+      };
+    });
+  }
+}
 
 final dataService = DataService();
 
@@ -70,81 +130,73 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
-
-
   @override
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-
-      debugShowCheckedModeBanner:false,
-
+      theme: ThemeData(primarySwatch: Colors.grey),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-
-        appBar: AppBar( 
-
+        appBar: AppBar(
           title: const Text("Dicas"),
-
-          ),
-
-        body: ValueListenableBuilder(
-
-          valueListenable: dataService.tableStateNotifier,
-
-          builder:(_, value, __){
-
-            switch (value['status']){
-
-              case TableStatus.idle: 
-
-                return Text("Toque algum botão");
-
-              case TableStatus.loading:
-
-                return CircularProgressIndicator();
-
-              case TableStatus.ready: 
-
-                return DataTableWidget(
-
-                  jsonObjects: value['dataObjects'], 
-
-                  propertyNames: value['propertyNames'], 
-
-                  columnNames: ["Nome", "Estilo", "IBU"]
-
-                );
-
-              case TableStatus.error: 
-
-                return Text("Lascou");
-
-            }
-
-            return Text("...");
-
-            
-
-          }
-
         ),
-
+        body: ValueListenableBuilder(
+          valueListenable: dataService.tableStateNotifier,
+          builder: (_, value, __) {
+            switch (value['status']) {
+              case TableStatus.idle:
+                return Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        'https://www.bemcolar.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/l/e/letreiro-3d-decorativo-bem-vindo.jpg',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 200),
+                          Text(
+                            "Bem-vindo ao meu aplicativo!\nToque em um botão para começar.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              case TableStatus.loading:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              case TableStatus.ready:
+                return DataTableWidget(
+                  jsonObjects: value['dataObjects'],
+                  propertyNames: ["name", "style", "ibu"],
+                  columnNames: value['columnNames'],
+                );
+              case TableStatus.error:
+                return Text("Ocorreu um erro");
+              default:
+                return Text("...");
+            }
+          },
+        ),
         bottomNavigationBar: NewNavBar(itemSelectedCallback: dataService.carregar),
-
-      ));
-
+      ),
+    );
   }
-
 }
 
 class NewNavBar extends HookWidget {
-  final _itemSelectedCallback;
+  final Function(int) itemSelectedCallback;
 
-  NewNavBar({itemSelectedCallback}) : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
+  NewNavBar({required this.itemSelectedCallback});
 
   @override
   Widget build(BuildContext context) {
@@ -153,7 +205,7 @@ class NewNavBar extends HookWidget {
     return BottomNavigationBar(
       onTap: (index) {
         state.value = index;
-        _itemSelectedCallback(index);
+        itemSelectedCallback(index);
       },
       currentIndex: state.value,
       items: const [
