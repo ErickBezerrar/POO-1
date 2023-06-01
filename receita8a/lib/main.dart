e62fa36a0a0e82f19e06baa6a18dc2fd9721d03c
ErickBezerrar <erickbrtrindade@gmail.com>
2023-06-01 13:18:21 -0300

    receita8a -> scroll infinito

--- receita8a/lib/main.dart
+++ receita8a/lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:provider/provider.dart';
+
 import 'package:http/http.dart' as http;
+
 import 'dart:convert';
 
 
-enum TableStatus { idle, loading, ready, error }
 
-enum ItemType { beer, coffee, nation, none }
+enum TableStatus{idle,loading,ready,error}
+
+enum ItemType{beer, coffee, nation, none}
+
+
 
 class DataService {
-  final ValueNotifier<Map<String, dynamic>> tableStateNotifier = ValueNotifier({
+  final ValueNotifier<Map<String, dynamic>> tableStateNotifier =
+      ValueNotifier({
     'status': TableStatus.idle,
     'dataObjects': [],
     'itemType': ItemType.none,
@@ -54,8 +60,10 @@ class DataService {
     List<String> propertyNames,
     List<String> columnNames,
   ) async {
+    // Ignorar solicitação se uma requisição já estiver em curso
     if (tableStateNotifier.value['status'] == TableStatus.loading) return;
 
+    // Emitir estado de loading se items em exibição não forem do mesmo tipo
     if (tableStateNotifier.value['itemType'] != itemType) {
       tableStateNotifier.value = {
         'status': TableStatus.loading,
@@ -76,6 +84,7 @@ class DataService {
       final jsonObjects = jsonDecode(jsonString);
 
       if (tableStateNotifier.value['status'] != TableStatus.loading) {
+        // Se já houver itens no estado da tabela, mescla os novos itens com os existentes
         jsonObjects.addAll(tableStateNotifier.value['dataObjects']);
       }
 
@@ -98,171 +107,271 @@ class DataService {
 
 final dataService = DataService();
 
+
+
 void main() {
+
   MyApp app = MyApp();
 
   runApp(app);
+
 }
 
 class MyApp extends StatelessWidget {
+
   final functionsMap = {
+
     ItemType.beer: dataService.carregarCervejas,
+
     ItemType.coffee: dataService.carregarCafes,
+
     ItemType.nation: dataService.carregarNacoes
+
   };
 
   @override
+
   Widget build(BuildContext context) {
+
     return MaterialApp(
+
+      
+
       theme: ThemeData(primarySwatch: Colors.deepPurple),
-      debugShowCheckedModeBanner: false,
+
+      debugShowCheckedModeBanner:false,
+
       home: Scaffold(
-        appBar: AppBar(
-          title: Builder(
-            builder: (context) {
-              final value = context.watch<DataService>().tableStateNotifier.value;
-              final itemCount = value['dataObjects'].length;
-              return Row(
-                children: [
-                  const Text("Dicas"),
-                  const SizedBox(width: 10),
-                  Text('($itemCount itens)'),
-                ],
-              );
-            },
+
+        appBar: AppBar( 
+
+          title: const Text("Dicas"),
+
           ),
-        ),
+
         body: ValueListenableBuilder(
+
           valueListenable: dataService.tableStateNotifier,
-          builder: (_, value, __) {
-            switch (value['status']) {
-              case TableStatus.idle:
+
+          builder:(_, value, __){       
+
+            
+
+            switch (value['status']){
+
+              case TableStatus.idle: 
+
                 return Center(child: Text("Toque algum botão, abaixo..."));
 
               case TableStatus.loading:
+
                 return Center(child: CircularProgressIndicator());
 
-              case TableStatus.ready:
+              case TableStatus.ready: 
+
                 return ListWidget(
-                  jsonObjects: value['dataObjects'],
-                  propertyNames: value['propertyNames'],
+
+                  jsonObjects: value['dataObjects'], 
+
+                  propertyNames: value['propertyNames'], 
+
                   scrollEndedCallback: functionsMap[value['itemType']],
+
                 );
 
-              case TableStatus.error:
+              case TableStatus.error: 
+
                 return Text("Lascou");
+
             }
 
             return Text("...");
-          },
+
+          }
+
         ),
-        bottomNavigationBar: NewNavBar(
-          itemCount: functionsMap.length,
-          itemSelectedCallback: dataService.carregar,
-        ),
-      ),
-    );
+
+        bottomNavigationBar: NewNavBar(itemSelectedCallback: dataService.carregar),
+
+      ));
+
   }
+
 }
 
 
 class NewNavBar extends HookWidget {
-  final _itemSelectedCallback;
-  final int itemCount;
 
-  NewNavBar({itemSelectedCallback, required this.itemCount})
-      : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
+  final _itemSelectedCallback;
+
+  NewNavBar({itemSelectedCallback}):
+
+    _itemSelectedCallback = itemSelectedCallback ?? (int){}
 
   @override
+
   Widget build(BuildContext context) {
+
     var state = useState(1);
 
     return BottomNavigationBar(
-        onTap: (index) {
-          state.value = index;
 
-          _itemSelectedCallback(index);
-        },
-        currentIndex: state.value,
-        items: [
-          BottomNavigationBarItem(
-            label: "Cafés",
-            icon: Icon(Icons.coffee_outlined),
-          ),
-          BottomNavigationBarItem(
-              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-          BottomNavigationBarItem(
-              label: "Nações", icon: Icon(Icons.flag_outlined))
-        ].take(itemCount).toList(),
-    );
+      onTap: (index){
+
+        state.value = index;
+
+        _itemSelectedCallback(index);                
+
+      }, 
+
+      currentIndex: state.value,
+
+      items: const [
+
+        BottomNavigationBarItem(
+
+          label: "Cafés",
+
+          icon: Icon(Icons.coffee_outlined),
+
+        ),
+
+        BottomNavigationBarItem(
+
+            label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+
+        BottomNavigationBarItem(
+
+          label: "Nações", icon: Icon(Icons.flag_outlined))
+
+      ]);
+
   }
+
 }
 
 
+
+
+
 class ListWidget extends HookWidget {
+
   final dynamic _scrollEndedCallback;
 
   final List jsonObjects;
 
   final List<String> propertyNames;
 
-  ListWidget(
-      {this.jsonObjects = const [],
-      this.propertyNames = const [],
-      void Function()? scrollEndedCallback})
-      : _scrollEndedCallback = scrollEndedCallback ?? false;
+  ListWidget( {this.jsonObjects = const [], this.propertyNames= const [], void Function()? scrollEndedCallback }):
+
+    _scrollEndedCallback = scrollEndedCallback ?? false;
 
   @override
+
   Widget build(BuildContext context) {
+
     var controller = useScrollController();
 
-    useEffect(() {
-      controller.addListener(
-        () {
-          if (controller.position.pixels ==
-              controller.position.maxScrollExtent) {
-            print('end reached');
+    useEffect(
 
-            if (_scrollEndedCallback is Function) _scrollEndedCallback();
-          }
-        },
-      );
-    }, [controller]);
+      (){
 
-    return ListView.separated(
+        controller.addListener(
+
+          (){
+
+            if (controller.position.pixels == controller.position.maxScrollExtent){
+
+              print('end reached');
+
+              if (_scrollEndedCallback is Function)
+
+                _scrollEndedCallback();
+
+            }
+
+          },
+
+        );
+
+      },[controller]
+
+    );
+
+    return ListView.separated(    
+
       controller: controller,
+
       padding: EdgeInsets.all(10),
-      separatorBuilder: (_, __) => Divider(
-        height: 5,
-        thickness: 2,
-        indent: 10,
-        endIndent: 10,
-        color: Theme.of(context).primaryColor,
-      ),
-      itemCount: jsonObjects.length + 1,
-      itemBuilder: (_, index) {
-        if (index == jsonObjects.length)
+
+      separatorBuilder: (_,__) => Divider(
+
+            height: 5,
+
+            thickness: 2,
+
+            indent: 10,
+
+            endIndent: 10,
+
+            color: Theme.of(context).primaryColor,
+
+          ),
+
+      itemCount: jsonObjects.length+1,
+
+      itemBuilder: (_, index){
+
+        if (index==jsonObjects.length)
+
           return Center(child: LinearProgressIndicator());
 
         var title = jsonObjects[index][propertyNames[0]];
 
         var content = propertyNames
-            .sublist(1)
-            .map((prop) => jsonObjects[index][prop])
-            .join(" - ");
 
-        return Card(
-            shadowColor: Theme.of(context).primaryColor,
-            child: Column(children: [
+                        .sublist(1)
+
+                        .map( (prop) => jsonObjects[index][prop] )
+
+                        .join(" - ");
+
+        return Card(     
+
+          shadowColor: Theme.of(context).primaryColor,     
+
+          child: Column( 
+
+            children: [
+
               SizedBox(height: 10),
 
-              Text("${title}\n", style: TextStyle(fontWeight: FontWeight.bold)),
+              //a primeira propriedade vai em negrito
+
+              Text(                    
+
+                "${title}\n",
+
+                 style: TextStyle(fontWeight: FontWeight.bold)
+
+              ),
+
+              //as demais vão normais
 
               Text(content),
 
               SizedBox(height: 10)
-            ]));
+
+            ]
+
+          )
+
+        ); 
+
       },
+
     );
+
   }
+  
 }
