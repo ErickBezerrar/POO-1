80c3a432f6c7f416e03dce6e46f7c2f8039b5090
ErickBezerrar <erickbrtrindade@gmail.com>
2023-07-09 13:58:04 -0300

    ordenação

--- receita10c2/lib/view/widgets.dart
+++ receita10c2/lib/view/widgets.dart
@@ -6,47 +6,52 @@ class MyApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(kToolbarHeight),
-            child: MyAppBar(callback: dataService.filtrarEstadoAtual),
-          ),
-          body: ValueListenableBuilder(
-              valueListenable: dataService.tableStateNotifier,
-              builder: (_, value, __) {
-                switch (value['status']) {
-                  case TableStatus.idle:
-                    return Center(child: Text("Toque em algum botão"));
+      theme: ThemeData(
+        primaryColor: Colors.blue, // Altera a cor primária para azul
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: MyAppBar(callback: dataService.filtrarEstadoAtual),
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: dataService.tableStateNotifier,
+          builder: (_, value, __) {
+            switch (value['status']) {
+              case TableStatus.idle:
+                return Center(child: Text("Toque em algum botão"));
 
-                  case TableStatus.loading:
-                    return Center(child: CircularProgressIndicator());
+              case TableStatus.loading:
+                return Center(child: CircularProgressIndicator());
 
-                  case TableStatus.ready:
-                    return Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        child: SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: DataTableWidget(
-                                jsonObjects: value['dataObjects'],
-                                propertyNames: value['propertyNames'],
-                                columnNames: value['columnNames'])),
+              case TableStatus.ready:
+                return Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: DataTableWidget(
+                        jsonObjects: value['dataObjects'],
+                        propertyNames: value['propertyNames'],
+                        columnNames: value['columnNames'],
                       ),
-                    );
+                    ),
+                  ),
+                );
 
-                  case TableStatus.error:
-                    return Text("Lascou");
-                }
+              case TableStatus.error:
+                return Text("Lascou");
+            }
 
-                return Text("...");
-              }),
-          bottomNavigationBar:
-              NewNavBar(itemSelectedCallback: dataService.carregar),
-        ));
+            return Text("...");
+          },
+        ),
+        bottomNavigationBar: NewNavBar(itemSelectedCallback: dataService.carregar),
+      ),
+    );
   }
 }
 
@@ -61,22 +66,27 @@ class NewNavBar extends HookWidget {
     var state = useState(1);
 
     return BottomNavigationBar(
-        onTap: (index) {
-          state.value = index;
+      onTap: (index) {
+        state.value = index;
 
-          _itemSelectedCallback(index);
-        },
-        currentIndex: state.value,
-        items: const [
-          BottomNavigationBarItem(
-            label: "Cafés",
-            icon: Icon(Icons.coffee_outlined),
-          ),
-          BottomNavigationBarItem(
-              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-          BottomNavigationBarItem(
-              label: "Nações", icon: Icon(Icons.flag_outlined))
-        ]);
+        _itemSelectedCallback(index);
+      },
+      currentIndex: state.value,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined, color: Colors.green), // Altera a cor do ícone para verde
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas",
+          icon: Icon(Icons.local_drink_outlined, color: Colors.blue), // Altera a cor do ícone para azul
+        ),
+        BottomNavigationBarItem(
+          label: "Nações",
+          icon: Icon(Icons.flag_outlined, color: Colors.red), // Altera a cor do ícone para vermelho
+        ),
+      ],
+    );
   }
 }
 
@@ -85,10 +95,11 @@ class DataTableWidget extends HookWidget {
   final List<String> columnNames;
   final List<String> propertyNames;
 
-  DataTableWidget(
-      {this.jsonObjects = const [],
-      this.columnNames = const [],
-      this.propertyNames = const []});
+  DataTableWidget({
+    this.jsonObjects = const [],
+    this.columnNames = const [],
+    this.propertyNames = const [],
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -96,27 +107,49 @@ class DataTableWidget extends HookWidget {
     final sortColumnIndex = useState(1);
 
     return DataTable(
-        sortAscending: sortAscending.value,
-        sortColumnIndex: sortColumnIndex.value,
-        columns: columnNames
-            .map((name) => DataColumn(
-                onSort: (columnIndex, ascending) {
-                  sortColumnIndex.value = columnIndex;
-                  sortAscending.value = !sortAscending.value;
+      sortAscending: sortAscending.value,
+      sortColumnIndex: sortColumnIndex.value,
+      dataRowColor: MaterialStateColor.resolveWith((states) => Colors.grey), // Altera a cor de fundo das células para cinza claro
+      columns: columnNames
+          .map(
+            (name) => DataColumn(
+              onSort: (columnIndex, ascending) {
+                sortColumnIndex.value = columnIndex;
+                sortAscending.value = !sortAscending.value;
 
-                  dataService.ordenarEstadoAtual(
-                      propertyNames[columnIndex], sortAscending.value);
-                },
-                label: Expanded(
-                    child: Text(name,
-                        style: TextStyle(fontStyle: FontStyle.italic)))))
-            .toList(),
-        rows: jsonObjects
-            .map((obj) => DataRow(
-                cells: propertyNames
-                    .map((propName) => DataCell(Text(obj[propName])))
-                    .toList()))
-            .toList());
+                dataService.ordenarEstadoAtual(propertyNames[columnIndex], sortAscending.value);
+              },
+              label: Expanded(
+                child: Text(
+                  name,
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black, // Altera a cor do texto da coluna para preto
+                  ),
+                ),
+              ),
+            ),
+          )
+          .toList(),
+      rows: jsonObjects
+          .map(
+            (obj) => DataRow(
+              cells: propertyNames
+                  .map(
+                    (propName) => DataCell(
+                      Text(
+                        obj[propName],
+                        style: TextStyle(
+                          color: Colors.black, // Altera a cor do texto das células para preto
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          )
+          .toList(),
+    );
   }
 }
 
@@ -129,30 +162,35 @@ class MyAppBar extends HookWidget {
   Widget build(BuildContext context) {
     var state = useState(7);
 
-    return AppBar(actions: [
-      Flexible(
-        child: TextField(
-          onChanged: (value) {
-            _callback(value);
-          },
-          decoration: const InputDecoration(
-            hintText: 'Digite algo...',
+    return AppBar(
+      backgroundColor: Colors.orange, 
+      actions: [
+        Flexible(
+          child: TextField(
+            onChanged: (value) {
+              _callback(value);
+            },
+            cursorColor: Colors.orange, // Altera a cor do cursor para laranja
+            style: TextStyle(color: Colors.black), // Altera a cor do texto para preto
+            decoration: const InputDecoration(
+              hintText: 'Digite algo...',
+            ),
           ),
         ),
-      ),
-      PopupMenuButton(
-        initialValue: state.value,
-        itemBuilder: (_) => valores
-            .map((num) => PopupMenuItem(
-                  value: num,
-                  child: Text("Carregar $num itens por vez"),
-                ))
-            .toList(),
-        onSelected: (number) {
-          state.value = number;
-          dataService.numberOfItems = number;
-        },
-      )
-    ]);
+        PopupMenuButton(
+          initialValue: state.value,
+          itemBuilder: (_) => valores.map(
+            (num) => PopupMenuItem(
+              value: num,
+              child: Text("Carregar $num itens por vez"),
+            ),
+          ).toList(),
+          onSelected: (number) {
+            state.value = number;
+            dataService.numberOfItems = number;
+          },
+        ),
+      ],
+    );
   }
 }
